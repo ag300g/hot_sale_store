852a3b833716ad5d7afdbbba75f17d928f10b3c4
ag300g <ag300g@163.com>
2018-09-10 16:59:31 +0800

    Create m01_sku_selection.py

---
+++ 02.model/m01_sku_selection.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+==============================================
+function
+----------------------------------------------
+
+         FILE: m01_sku_selection.py
+
+  DESCRIPTION: sku selection
+       TARGET: select n sku satisfy the alpha * M order
+       OPTIONS:
+            inputs:
+                # ss_data: sale and stock data
+                # alpha: the sale scale to be satisfied
+        NOTES:
+            # ss_data is a DataFrame must contain the following columns:
+                    - sku_id
+                    - sale_30_avg: mean of latest 30 days sales
+                    - canuse_qtty: canuse_stock
+                    - sale_3_avg: mean of latest 3 day sales
+            # alpha must in [0,1]
+ REQUIREMENTS: ---
+       AUTHOR: 
+      VERSION: 1.0
+      CREATED: 
+       MODIFY: ---
+=================================================
+"""
+
+from auxiliary import fileManagement as fm
+import numpy as np
+from sklearn import preprocessing
+import pandas as pd
+
+
+from auxiliary import getRunningEnvironment
+
+import datetime as dt
+from textwrap import wrap
+import copy
+import os
+import shutil
+
+from matplotlib.font_manager import FontProperties
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+import pandas as pd
+
+import scipy.stats
+
+from scipy.optimize import curve_fit
+from numpy import inf
+
+import warnings
+warnings.simplefilter('ignore', np.RankWarning)
+
+
+
+def main(scenario):
+
+
+
+
+if __name__ == '__main__':
+    scenarioFileName = 'settings_scenario_hot_sku_store.yaml'
+    settingsScenario = fm.loadSettingsFromYamlFile(scenarioFileName)
+    main(settingsScenario)
