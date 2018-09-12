826670eefca3403eae33da6bfc42f4260022cdae
ag300g <ag300g@163.com>
2018-09-12 18:25:36 +0800

    update 0912

--- 02.model/m02_sale_division.py
+++ 02.model/m02_sale_division.py
@@ -38,17 +38,15 @@ from textwrap import wrap
 import copy
 import os
 import shutil
-
 from matplotlib.font_manager import FontProperties
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-
 import pandas as pd
-import numpy as np
 import scipy.stats
-
 from scipy.optimize import curve_fit
 from numpy import inf
+
+import numpy as np
 import yaml
 import platform
 
@@ -79,7 +77,22 @@ def genFakesale(scenario):
     selected_sku_id = sku_id[:int(scenario['skuNum']*0.2)]
     unselected_sku_id = sku_id[int(scenario['skuNum']*0.2):]
     fake_sku_id = selected_sku_id*16+unselected_sku_id
-    return {'fake_sku_id':np.array(fake_sku_id), 'selected_sku_id':selected_sku_id, 'unselected_sku_id':unselected_sku_id}
+    return {'sku_id': np.array(sku_id) 'fake_sku_id': np.array(fake_sku_id), 'selected_sku_id': np.array(selected_sku_id), 'unselected_sku_id': np.array(unselected_sku_id)}
+
+
+def genFakePartition(scenario):
+    '''
+    need genFakesale
+    :param scenario:
+    :return:
+    '''
+    selected_sku_id = genFakesale(scenario)['selected_sku_id']
+    unselected_sku_id = genFakesale(scenario)['unselected_sku_id']
+    sku_in_store_1 = np.append(selected_sku_id[:int(len(selected_sku_id) / 2)],
+                               unselected_sku_id[:int(len(unselected_sku_id) / 2)])  ## small half to store 1
+    sku_in_store_2 = np.setdiff1d(np.append(selected_sku_id, unselected_sku_id), store_1, assume_unique=True)
+    return { 'sku_in_store_1': sku_in_store_1, 'sku_in_store_2': sku_in_store_2 }
+
 
 
 def genFakeOrder(scenario):
@@ -99,17 +112,98 @@ def genFakeOrder(scenario):
         boldM[unique,i] = counts
     return boldM
 
+def matrixDegenerate(boldM,selected_sku_id,sku_in_store_1):
+    '''
+    :param boldM:
+    :param sku_selection: the left are unselected skus
+    :param sku_in_store_1: the left skus are in store 2
+    :return:
+    '''
+    sku_id = [i for i in range(boldM.shape[0])]  ## the row index is the sku_id
+    unselected_sku_id = np.setdiff1d(np.array(sku_id), selected_sku_id, assume_unique=True)
+    sku_in_store_2 = np.setdiff1d(np.array(sku_id), sku_in_store_1, assume_unique=True)
+    unselected_store_1_sku_id = np.intersect1d(unselected_sku_id, sku_in_store_1, assume_unique=True)
+    unselected_store_2_sku_id = np.intersect1d(unselected_sku_id, sku_in_store_2, assume_unique=True)
+    selected_store_1_sku_id = np.intersect1d(selected_sku_id, sku_in_store_1, assume_unique=True)
+    selected_store_2_sku_id = np.intersect1d(selected_sku_id, sku_in_store_2, assume_unique=True)
+
+    ## calculate the Fake rows for order division
+    unselected_store_1_result = boldM[unselected_store_1_sku_id,:].sum(axis=0, keepdims=True)
+    unselected_store_2_result = boldM[unselected_store_2_sku_id,:].sum(axis=0, keepdims=True)
+    selected_store_1_result = boldM[selected_store_1_sku_id,:].sum(axis=0, keepdims=True)
+    selected_store_2_result = boldM[selected_store_2_sku_id,:].sum(axis=0, keepdims=True)
+
+    deBoldM = np.concatenate((selected_store_1_result, selected_store_2_result, unselected_store_1_result, unselected_store_2_result), axis=0)
+    deBoldM_rowname = ['selected_store_1_result','selected_store_2_result','unselected_store_1_result','unselected_store_2_result']
+
+    return {'deBoldM_rowname':deBoldM_rowname, 'deBoldM':deBoldM}
+
+
+def ordMark(deBoldM):
+    '''
+    :param deBoldM:
+    :return: logical np.array
+    '''
+    cond1 = (deBoldM[2,:] > 0) & (deBoldM[3,:] > 0)
+    cond2 = (deBoldM[0,:] == 0) & (deBoldM[1,:] > 0) & (deBoldM[2,:] == 0) & (deBoldM[3,:] > 0)
+    cond3 = (deBoldM[0,:] > 0) & (deBoldM[1, :] == 0) & (deBoldM[2, :] > 0) & (deBoldM[3, :] == 0)
+    false_y = np.logical_or(cond1,cond2,cond3)
+    # false_y = cond1 | cond2 |cond3
+
+    return false_y
+
+
+
+
+
+
+
 
 '''
+M = scenario['skuNum']
+
+
 a = np.array([0, 3, 0, 1, 0, 1, 2, 1, 0, 0, 0, 0, 1, 3, 4])
 unique, counts = np.unique(a, return_counts=True)
 dict(zip(unique, counts))
+
+Z = np.arange(12).reshape(3,4)
+Z[np.array([False,True,True]),:]
+
+
+
+if d3 > 0 and d4 > 0:
+    y = 0
+elif d1 > 0 and d2 == 0 and d3 > 0 and d4 == 0:
+    y = 0
+elif d1 == 0 and d2 > 0 and d3 == 0 and d4 > 0:
+    y = 0
+else:
+    y = 1
+
+a = np.array([True, False, False])
+b = np.array([False, True, False])
+c = np.array([True, True, True])
+d = np.array([False, False, False])
+
+a & b & d
+a | b | c
+
+
+
 '''
 
 def main(scenario):
     boldM = genFakeOrder(scenario)
-    M = scenario['skuNum']
-    store_label = genFakesale(scenario)['selected_sku_id']
+    selected_sku_id = genFakesale(scenario)['selected_sku_id']
+    sku_in_store_1 = genFakePartition(scenario)['sku_in_store_1']
+    deBoldMAll = matrixDegenerate(boldM,selected_sku_id,sku_in_store_1)
+    deBoldM = deBoldMAll['deBoldM']
+    deBoldM_rowname = deBoldMAll['deBoldM_rowname']
+    print(deBoldM_rowname)
+    false_y = ordMark(deBoldM)
+
+
 
 
 
